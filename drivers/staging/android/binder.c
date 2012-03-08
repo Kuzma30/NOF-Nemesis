82e1c8173f206326f9379cf5e6456ffe0b5905f9
Arve Hjønnevåg <arve@android.com>
2012-03-08 15:43:36 -0800

    Staging: android: binder: Fix use-after-free bug
    binder_update_page_range could read freed memory if the vma of the
    selected process was freed right before the check that the vma
    belongs to the mm struct it just locked.
    
    If the vm_mm pointer in that freed vma struct had also been rewritten
    with a value that matched the locked mm struct, then the code would
    proceed and possibly modify the freed vma.
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: stratosk <stratosk@semaphore.gr>
    
    Conflicts:
    	drivers/staging/android/binder.c
    
    Change-Id: Ief9b09b1e091b5d0d23f9accab64e2830e2513de

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -287,6 +287,7 @@ struct binder_proc {
 	struct rb_root refs_by_node;
 	int pid;
 	struct vm_area_struct *vma;
+	struct mm_struct *vma_vm_mm;
 	struct task_struct *tsk;
 	struct files_struct *files;
 	struct hlist_node deferred_work_node;
@@ -631,6 +632,14 @@ static int binder_update_page_range(struct binder_proc *proc, int allocate,
 	if (mm) {
 		down_write(&mm->mmap_sem);
 		vma = proc->vma;
+<<<<<<< HEAD
+=======
+		if (vma && mm != proc->vma_vm_mm) {
+			pr_err("binder: %d: vma mm and task mm mismatch\n",
+				proc->pid);
+			vma = NULL;
+		}
+>>>>>>> 4c38ca5... Staging: android: binder: Fix use-after-free bug
 	}
 
 	if (allocate == 0)
@@ -2793,6 +2802,7 @@ static void binder_vma_close(struct vm_area_struct *vma)
 		     (vma->vm_end - vma->vm_start) / SZ_1K, vma->vm_flags,
 		     (unsigned long)pgprot_val(vma->vm_page_prot));
 	proc->vma = NULL;
+	proc->vma_vm_mm = NULL;
 	binder_defer_work(proc, BINDER_DEFERRED_PUT_FILES);
 }
 
@@ -2873,6 +2883,7 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 	barrier();
 	proc->files = get_files_struct(current);
 	proc->vma = vma;
+	proc->vma_vm_mm = vma->vm_mm;
 
 	/*printk(KERN_INFO "binder_mmap: %d %lx-%lx maps %p\n",
 		 proc->pid, vma->vm_start, vma->vm_end, proc->buffer);*/
