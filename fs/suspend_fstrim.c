636f036afcc905f4c884defaef58ffecb44e3d40
Boy Petersen <boypetersen@gmail.com>
2013-03-03 14:51:43 +0100

    trim /cache partition on suspend if the partition has not been trimmed for more than 15 minutes requires fstrim binary in /sbin directory
    Conflicts:
    	fs/suspend_fstrim.c
    	init/main.c
    
    Change-Id: Id9533bc5f06956ac39cca8beb7448cded8ffdbf8

--- fs/suspend_fstrim.c
+++ fs/suspend_fstrim.c
@@ -4,6 +4,7 @@
 #include <linux/earlysuspend.h>
 #include <linux/fs.h>
 
+<<<<<<< HEAD
 static u64 resumejiffies;
 
 static void fstrim_early_suspend(struct early_suspend *handler)
@@ -13,6 +14,13 @@ static void fstrim_early_suspend(struct early_suspend *handler)
 	awakejiffies += (jiffies - resumejiffies);
 
 	if (awakejiffies >= 360 * HZ) {
+=======
+static void fstrim_early_suspend(struct early_suspend *handler)
+{
+	static unsigned long lasttrimjiffies;
+
+	if (jiffies - lasttrimjiffies >= 900 * HZ) {
+>>>>>>> 7625a1a... trim /cache partition on suspend if the partition has not been trimmed for more than 15 minutes
 		char *argv[] = { "/sbin/fstrim", "/cache", NULL };
 		static char *envp[] = {
         		"HOME=/",
@@ -21,6 +29,7 @@ static void fstrim_early_suspend(struct early_suspend *handler)
 
 		call_usermodehelper( argv[0], argv, envp, UMH_WAIT_EXEC );
 
+<<<<<<< HEAD
 		awakejiffies = 0;
 	}
 }
@@ -33,6 +42,14 @@ static void fstrim_late_resume(struct early_suspend *handler)
 static struct early_suspend suspend_fstrim = {
 	.suspend = fstrim_early_suspend,
 	.resume = fstrim_late_resume,
+=======
+		lasttrimjiffies = jiffies;
+	}
+}
+
+static struct early_suspend suspend_fstrim = {
+	.suspend = fstrim_early_suspend,
+>>>>>>> 7625a1a... trim /cache partition on suspend if the partition has not been trimmed for more than 15 minutes
 };
 
 void __init suspend_fstrim_init(void)
