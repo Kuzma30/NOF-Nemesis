2cdeb5a064877d65d7018ac4e1e54140908ac559
demetris <demetris@walla.com>
2013-05-04 18:35:34 +0300

    lets try Under Volting and stability
    Change-Id: I30e77080e32604a2c81987e4363cb83f6f911f5e

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -89,11 +89,11 @@ struct omap4_ldo_abb_trim_data {
  * voltage dependent data for each VDD.
  */
 
-#define OMAP4430_VDD_MPU_OPP50_UV		1025000
-#define OMAP4430_VDD_MPU_OPP100_UV		1200000
-#define OMAP4430_VDD_MPU_OPPTURBO_UV		1325000
-#define OMAP4430_VDD_MPU_OPPNITRO_UV		1388000
-#define OMAP4430_VDD_MPU_OPPNITROSB_UV		1398000
+#define OMAP4430_VDD_MPU_OPP50_UV       975000    /*  300 */
+#define OMAP4430_VDD_MPU_OPP100_UV      1145000    /*  600 */
+#define OMAP4430_VDD_MPU_OPPTURBO_UV    1265000    /*  800 */
+#define OMAP4430_VDD_MPU_OPPNITRO_UV    1325000    /* 1080 */
+#define OMAP4430_VDD_MPU_OPPNITROSB_UV  1345000    /* 1350 */
 
 
 struct omap_volt_data omap443x_vdd_mpu_volt_data[] = {
@@ -105,9 +105,9 @@ struct omap_volt_data omap443x_vdd_mpu_volt_data[] = {
 	VOLT_DATA_DEFINE(0, 0, 0, 0, 0, 0),
 };
 
-#define OMAP4430_VDD_IVA_OPP50_UV		 950000
-#define OMAP4430_VDD_IVA_OPP100_UV		1114000
-#define OMAP4430_VDD_IVA_OPPTURBO_UV		1291000
+#define OMAP4430_VDD_IVA_OPP50_UV		 990000
+#define OMAP4430_VDD_IVA_OPP100_UV		1165000
+#define OMAP4430_VDD_IVA_OPPTURBO_UV	1275000
 
 struct omap_volt_data omap443x_vdd_iva_volt_data[] = {
 	VOLT_DATA_DEFINE(OMAP4430_VDD_IVA_OPP50_UV, 0, OMAP44XX_CONTROL_FUSE_IVA_OPP50, 0xf4, 0x0c, OMAP_ABB_NOMINAL_OPP),
@@ -116,8 +116,8 @@ struct omap_volt_data omap443x_vdd_iva_volt_data[] = {
 	VOLT_DATA_DEFINE(0, 0, 0, 0, 0, 0),
 };
 
-#define OMAP4430_VDD_CORE_OPP50_UV		 962000
-#define OMAP4430_VDD_CORE_OPP100_UV		1127000
+#define OMAP4430_VDD_CORE_OPP50_UV		1000000
+#define OMAP4430_VDD_CORE_OPP100_UV		1175000
 
 struct omap_volt_data omap443x_vdd_core_volt_data[] = {
 	VOLT_DATA_DEFINE(OMAP4430_VDD_CORE_OPP50_UV, 0, OMAP44XX_CONTROL_FUSE_CORE_OPP50, 0xf4, 0x0c, OMAP_ABB_NONE),
